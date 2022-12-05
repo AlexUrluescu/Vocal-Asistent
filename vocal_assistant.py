177fb21c2c8bc4a23be9da50c907905089fa7e00
Alexandre <alexurluescu23@gmail.com>
2022-12-05 22:41:01 +0200

    Write code for the methods

--- vocal_assistant.py
+++ vocal_assistant.py
@@ -9,34 +9,45 @@ class Assistent():
         
     def change_name(self, new_name):
         self.name = new_name
+        return "Now the assistent's name it's " + new_name
     
     
     def get_name(self):
-        return self.name
+        return "Hello there ! My name is " + self.name
     
     
     def change_gender(self, new_gender):
-        pass
+        self.gender = new_gender
+        return "The gender of the assitent was changed to " + new_gender
     
     
     def get_gender(self):
-        pass
+        return "The gender of the assistent " + self.name + " is " + self.gender
     
     
     def change_speech_speed(self, new_speed):
-        pass
+        self.speech_speed = new_speed
+        return "The speed was changet to " + new_speed
     
     
     def get_speech_speed(self):
-        pass
+        return "The speed is " + self.speech_speed
     
     
     def listen_for_request(self):
-        pass
+        return "Hello there ! My name is " + self.name + ", how can I help you ?"
     
     
     def execute_request(self):
-        pass
+        return "Your request will be execute"
     
     
 x = Assistent("John", "male", 3)
+
+# ----------- test ------------------
+
+print(x.change_name("Hellen"))
+print(x.change_gender("female"))
+print(x.get_gender())
+print(x.listen_for_request())
+print(x.execute_request())
