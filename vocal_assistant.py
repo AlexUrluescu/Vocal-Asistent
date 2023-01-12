e06a6fb96ff12774fc2b4a40ab4e2f6c7dd957c4
Alexandre <alexurluescu23@gmail.com>
2023-01-12 22:33:29 +0200

    vocal_assistant: create a property task.manager
    This property self.task.manager will have all the methods of the Class
    Task.
    
    Created a method run_task who will find what methos the  Vocal Assistant
    are going to use

--- vocal_assistant.py
+++ vocal_assistant.py
@@ -5,7 +5,9 @@ import pyttsx3
 import os
 from pynput.keyboard import Key, Controller
 from AppOpener import run
-
+from util import terminal_print
+import task 
+from datetime import date, datetime
 class Assistent():
     def __init__(self, name: str, gender: str, speech_speed: int = 125):
         self.name = name
@@ -14,19 +16,23 @@ class Assistent():
         self.engine_speak = pyttsx3.init()
         self.engine_speak.setProperty('rate', self.speech_speed)
         self.engine_keyboard = Controller()
+        self.task_manager = task.Task(self.name.lower())
 
     
     def write(self, text):
+        """function that writes a text on the screen"""
         sleep(2)
         self.engine_keyboard.type(text)
 
 
-
-    def speak(self, text):
+    def speak(self, text: str) -> None:
+        """ Function that converts a text to speak """
         self.engine_speak.say(text)
         self.engine_speak.runAndWait()
 
+
     def get_audio(self):
+        """Function that get the audio"""
         r = sr.Recognizer()
         with sr.Microphone() as source:
             audio = r.listen(source)
@@ -34,7 +40,7 @@ class Assistent():
 
             try:
                 said = r.recognize_google(audio)
-                print(said)
+                terminal_print(said)
             except Exception as e:
                 print("Exception: " + str(e))
                 said = "Sorry, could not undersand you"
@@ -53,38 +59,51 @@ class Assistent():
     
     def change_gender(self, new_gender):
         self.gender = new_gender
-        return "The gender of the assitent was changed to " + new_gender
+        terminal_print(f"The gender of the assitent was changed to {new_gender}")
     
     
     def get_gender(self):
-        return "The gender of the assistent " + self.name + " is " + self.gender
+        terminal_print(f"The gender of the assistent {self.name} is {self.gender}")
+        return self.gender
     
     
     def change_speech_speed(self, new_speed):
         self.speech_speed = new_speed
-        return "The speed was changet to " + new_speed
+        self.engine_speak.setProperty('rate', self.speech_speed)
+        terminal_print(f"The speed was changet to  + {new_speed}")
     
     
     def get_speech_speed(self):
-        return "The speed is " + self.speech_speed
+        terminal_print(f"The speed is {self.speech_speed}")
+        return self.speech_speed
     
-    
-    def listen_for_request(self):
-        return "Hello there ! My name is " + self.name + ", how can I help you ?"
-    
-    
-    def execute_request(self):
-        return "Your request will be execute"
-   
-    
-    def welcome(self):
-        return "You welcome"
 
-    
-    def like(self):
-        return "I like to help you and make you life easier"
+    def run_task(self, task_index):
+        if task_index == 0:
+            self.speak('What can I do for you?')
+        
+        if task_index == 1:
+            self.task_manager.open_app()
+        
+        if task_index == 2:
+            self.task_manager.next_slide()
+        
+        if task_index == 3:
+            self.task_manager.previous_slide()
 
+        if task_index == 4:
+            self.task_manager.get_temperature()
 
-    def kind_response(self):
-        return "This is my job, I will be always with you"
-    
+        if task_index == 5:
+            now = datetime.now()
+            ora = now.strftime("%H:%M:%S")
+            self.speak(ora)
+            self.task_manager.time()
+
+        if task_index == 6:
+            data = date.today()
+            self.speak(data)
+            self.task_manager.date(data)
+
+        if task_index == 7:
+            self.speak('You welcome')
